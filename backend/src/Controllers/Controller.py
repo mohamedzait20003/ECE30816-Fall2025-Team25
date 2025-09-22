46540bba8da90f90b81e8994d51b840a209aa8f2
Mohamed Mahmoud Zaitoun <155250402+mohamedzait20003@users.noreply.github.com>
2025-09-21 20:43:09 -0400

    Fixed all errors

--- backend/src/Controllers/Controller.py
+++ backend/src/Controllers/Controller.py
@@ -54,3 +54,20 @@ class Controller:
         except FileNotFoundError:
             print(f"File not found: {filepath}")
         return results
+
+
+if __name__ == "__main__":
+    fetcher = Controller()
+    dataset_links = [
+        "https://huggingface.co/datasets/xlangai/AgentNet",
+        "https://huggingface.co/datasets/osunlp/UGround-V1-Data",
+        "https://huggingface.co/datasets/xlangai/aguvis-stage2"
+    ]
+    code_link = "https://github.com/xlang-ai/OpenCUA"
+    model_link = "https://huggingface.co/xlangai/OpenCUA-32B"
+
+    model_data = fetcher.fetch(
+        model_link,
+        dataset_links=dataset_links,
+        code_link=code_link
+    )
